825dee58b178f5e022a2dc3042c44d10e7cb289d
Farshad <farshadjahanmanesh@gmail.com>
2023-07-21 22:28:10 +0200

    chore: fix xcode versions

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "MMDB",
+    products: [
+        .library(name: "MMDB",type: .dynamic, targets: ["MMDB"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "MMDB", dependencies: ["libmaxminddb"], publicHeadersPath: "."),
+        .testTarget(name: "MMDBTests", dependencies: ["MMDB"],exclude: ["Info-OSX.plist","Info-iOS.plist"]),
+        .target(name: "libmaxminddb", publicHeadersPath: "."),
+    ]
+)
